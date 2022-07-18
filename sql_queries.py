006908fe4b1390f9ea324b65c5aced95d02caa1c
rdvargas40 <34251165+rdvargas40@users.noreply.github.com>
2022-07-18 16:40:01 -0500

    Update Players Lambda
    Lambda que actualiza la tabla de jugadores

--- sql_queries.py
+++ sql_queries.py
@@ -24,27 +24,3 @@ def exec_sql_query(query: str):
     cur = conn.cursor()
     cur.execute(query)
     conn.close()
-
-
-# Creates the players table
-create_players_table = """--sql
-    CREATE TABLE IF NOT EXISTS players (
-        player_id INT PRIMARY KEY,
-        first_name VARCHAR NOT NULL,
-        last_name VARCHAR NOT NULL,
-        is_active BOOLEAN NOT NULL
-    );
-"""
-
-# Creates the teams table
-create_teams_table = """--sql
-    CREATE TABLE IF NOT EXISTS teams (
-        team_id INT PRIMARY KEY,
-        full_name VARCHAR NOT NULL,
-        abbreviation VARCHAR,
-        nickname VARCHAR,
-        city VARCHAR NOT NULL, -- If a city table is to be used, this needs to reference that table
-        state VARCHAR NOT NULL, -- The same for this field
-        year_founded INT,
-    );
-"""
