3c4049c8d4fa1c80990dc66d63cdfd6c14d89a07
rdvargas40 <34251165+rdvargas40@users.noreply.github.com>
2022-07-18 20:04:50 -0500

    Organize Players Matchs S3 Code
    Se deja solo una función que hace solo un intento.
    
    Eso si, imprime el porcentaje de éxito, junto con el listado de ids de los jugadores que no logro descargar

--- initialization/players_matchs.py
+++ initialization/players_matchs.py
@@ -16,6 +16,7 @@ AWS_KEY = config.get("AWS", "AWS_KEY")
 AWS_SECRET = config.get("AWS", "AWS_SECRET")
 AWS_REGION = config.get("AWS", "AWS_REGION")
 
+available_player_ids = [player['id'] for player in players.get_teams()]
 
 def get_write_player_matchs_historical_data(player_id: int) -> bool:
     """
@@ -49,11 +50,11 @@ def get_write_player_matchs_historical_data(player_id: int) -> bool:
     return False
 
 
-def populate_players_match_historical_data_from_list(player_id_list: List) -> List:
+def populate_players_match_historical_data_from_list(player_id_list: List=available_player_ids) -> List:
     """
     Get and write the historical data of a list opf players matchs.
     Args:
-        player_id_list (List): List of ids from the players whose data is to be downloaded and written into S3
+        player_id_list (List=available_player_ids): List of ids from the players whose data is to be downloaded and written into S3
     Returns:
         List of ids from the players at which the frunction failed to complete the operation
     """
@@ -64,29 +65,3 @@ def populate_players_match_historical_data_from_list(player_id_list: List) -> Li
             failed_players_ids.append(player_id)
     print(f'Success Rate: {1 - len(failed_players_ids)/len(player_id_list): .0%}')
     return failed_players_ids
-
-
-def populate_players_match_historical_data(loops: int=3) -> List:
-    """
-    Puplates the players match historical data in S3
-    Args:
-        loops=3 (int): Number of loops. The method will try again with those players that where left behind in the first attemps.
-    Returns:
-        List: Definite list of players for which the method was not able to get and write the historical data
-    """
-    
-    # Available Players in the NBA API
-    available_players = players.get_players()
-    available_players_ids = [p['id'] for p in available_players]
-    failed_players_ids = available_players_ids # At the start all the players are trated as failed
-    for _ in range(loops):
-        failed_players_ids = populate_players_match_historical_data_from_list(available_players_ids)
-        if len(failed_players_ids) == 0:
-            break
-        else:
-            available_players_ids = failed_players_ids
-    print('-'*50)
-    print(f'Overall Success Rate: {1 - len(failed_players_ids)/len(available_players): .0%}')
-    print('Failed to Populate Historical Data from the Following Players:')
-    return failed_players_ids
-    
