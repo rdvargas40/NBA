265cf92f1d31b78e0f20502027a3049ca3be3c0d
rdvargas40 <34251165+rdvargas40@users.noreply.github.com>
2022-07-20 11:23:13 -0500

    Tems Step Function
    Proceso Programado que se ejecuta periodicamente
    1. Actualiza los equipos
    2. Ejecuta en bucle las lambdas de actulización en S3 de los partidos de cada equipo

--- lambdas/update_players_matchs_s3/update_players_matchs_s3/app.py
+++ lambdas/update_players_matchs_s3/update_players_matchs_s3/app.py
@@ -3,13 +3,20 @@ from nba_api.stats.library.parameters import SeasonAll
 from nba_api.stats.endpoints import playergamelog
 
 
-def update_player_seasson_matchs_s3(player_id):
-    player_matchs_update = playergamelog.PlayerGameLog(player_id=player_id, season=SeasonAll.current_season)
+def update_player_season_matches_s3(player_id):
+    player_matchs_update = playergamelog.PlayerGameLog(
+        player_id=player_id, season=SeasonAll.current_season
+    )
     player_matchs_update_df = player_matchs_update.get_data_frames()[0]
 
     for season in player_matchs_update_df.SEASON_ID.unique():
-        player_matchs_season_df = player_matchs_update_df[player_matchs_update_df.SEASON_ID == season]
-        player_matchs_season_df.to_csv(f"s3://nba.pipeline/players/matchs/{player_id}/{season}.csv", index=False)
+        player_matchs_season_df = player_matchs_update_df[
+            player_matchs_update_df.SEASON_ID == season
+        ]
+        player_matchs_season_df.to_csv(
+            f"s3://nba.pipeline/players/matchs/{player_id}/{season}.csv",
+            index=False,
+        )
 
 
 def lambda_handler(event, context):
@@ -27,10 +34,12 @@ def lambda_handler(event, context):
     ------
     API Gateway Lambda Proxy Output Format: dict
     """
-    update_player_seasson_matchs_s3(event['player_id'])
+    update_player_season_matches_s3(event["player_id"])
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": f"Succeded at donwloading the data from: {event['first_name']} {event['last_name']}",
-        }),
+        "body": json.dumps(
+            {
+                "message": f"Succeded at donwloading the data from: {event['first_name']} {event['last_name']}",
+            }
+        ),
     }
